920f4a4699a3e41d930d3afe2f6176bfdb34c861
stania <stania.pe.kr@gmail.com>
2020-08-06 13:42:52 +0900

    added deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Usage: 
+# 	in ipojo-manip-*-logpresso branch
+# 		$ mvn clean install
+#	in ipojo-runtime-*-logpresso branch
+# 		$ cd core; mvn clean install
+# 	in ipojo-manip-*-logpresso branch
+# 		$ bash deploy.sh
+
+
+set -e
+
+JAR_DIR=./.targets
+REPO_VARS="-f deploy.xml -DrepositoryId=staging -Durl=sftp://staging.araqne.org/home/service/araqne -Dpackaging=jar"
+VER=`grep "version>.*<\/version" manipulator/pom.xml -B1 | grep -v -- "--" | xargs -n2 -d'\n' | grep manip | grep -o -P "(?<=version>).*(?=</version)"`
+
+# echo VER=$VER
+
+rm -rf $JAR_DIR
+mkdir -p $JAR_DIR
+
+cp core/target/org.apache.felix.ipojo-$VER.jar $JAR_DIR
+cp manipulator/target/org.apache.felix.ipojo.manipulator-$VER.jar $JAR_DIR
+cp annotations/target/org.apache.felix.ipojo.annotations-$VER.jar $JAR_DIR
+cp maven-ipojo-plugin/target/maven-ipojo-plugin-$VER.jar $JAR_DIR
+cp bnd-ipojo-plugin/target/bnd-ipojo-plugin-$VER.jar $JAR_DIR
+
+
+mvn deploy:deploy-file $REPO_VARS -DgroupId=org.apache.felix -DartifactId=org.apache.felix.ipojo -Dversion=$VER -Dfile=$JAR_DIR/org.apache.felix.ipojo-$VER.jar
+mvn deploy:deploy-file $REPO_VARS -DgroupId=org.apache.felix -DartifactId=org.apache.felix.ipojo.manipulator -Dversion=$VER -Dfile=$JAR_DIR/org.apache.felix.ipojo.manipulator-$VER.jar
+mvn deploy:deploy-file $REPO_VARS -DgroupId=org.apache.felix -DartifactId=org.apache.felix.ipojo.annotations -Dversion=$VER -Dfile=$JAR_DIR/org.apache.felix.ipojo.annotations-$VER.jar
+mvn deploy:deploy-file $REPO_VARS -DgroupId=org.apache.felix -DartifactId=maven-ipojo-plugin -Dversion=$VER -Dfile=$JAR_DIR/maven-ipojo-plugin-$VER.jar
+mvn deploy:deploy-file $REPO_VARS -DgroupId=org.apache.felix -DartifactId=bnd-ipojo-plugin -Dversion=$VER -Dfile=$JAR_DIR/bnd-ipojo-plugin-$VER.jar
