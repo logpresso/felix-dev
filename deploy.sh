0b6959e564916aec226299f99de0dcf6971db84e
stania <stania.pe.kr@gmail.com>
2020-09-08 23:09:35 +0900

    added ipojo-manipulator-bom in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@
 set -e
 
 JAR_DIR=./.targets
-REPO_VARS="-f deploy.xml -DrepositoryId=staging -Durl=sftp://staging.araqne.org/home/service/araqne -Dpackaging=jar"
+REPO_VARS="-f deploy.xml -DrepositoryId=staging -Durl=sftp://staging.araqne.org/home/service/araqne"
 VER=`grep "version>.*<\/version" manipulator/pom.xml -B1 | grep -v -- "--" | xargs -n2 -d'\n' | grep manip | grep -o -P "(?<=version>).*(?=</version)"`
 
 # echo VER=$VER
@@ -20,15 +20,16 @@ VER=`grep "version>.*<\/version" manipulator/pom.xml -B1 | grep -v -- "--" | xar
 rm -rf $JAR_DIR
 mkdir -p $JAR_DIR
 
+cp manipulator-bom/pom.xml $JAR_DIR/ipojo-manipulator-bom-$VER.xml
 cp core/target/org.apache.felix.ipojo-$VER.jar $JAR_DIR
 cp manipulator/target/org.apache.felix.ipojo.manipulator-$VER.jar $JAR_DIR
 cp annotations/target/org.apache.felix.ipojo.annotations-$VER.jar $JAR_DIR
 cp maven-ipojo-plugin/target/maven-ipojo-plugin-$VER.jar $JAR_DIR
 cp bnd-ipojo-plugin/target/bnd-ipojo-plugin-$VER.jar $JAR_DIR
 
-
-mvn deploy:deploy-file $REPO_VARS -DgroupId=org.apache.felix -DartifactId=org.apache.felix.ipojo -Dversion=$VER -Dfile=$JAR_DIR/org.apache.felix.ipojo-$VER.jar
-mvn deploy:deploy-file $REPO_VARS -DgroupId=org.apache.felix -DartifactId=org.apache.felix.ipojo.manipulator -Dversion=$VER -Dfile=$JAR_DIR/org.apache.felix.ipojo.manipulator-$VER.jar
-mvn deploy:deploy-file $REPO_VARS -DgroupId=org.apache.felix -DartifactId=org.apache.felix.ipojo.annotations -Dversion=$VER -Dfile=$JAR_DIR/org.apache.felix.ipojo.annotations-$VER.jar
-mvn deploy:deploy-file $REPO_VARS -DgroupId=org.apache.felix -DartifactId=maven-ipojo-plugin -Dversion=$VER -Dfile=$JAR_DIR/maven-ipojo-plugin-$VER.jar
-mvn deploy:deploy-file $REPO_VARS -DgroupId=org.apache.felix -DartifactId=bnd-ipojo-plugin -Dversion=$VER -Dfile=$JAR_DIR/bnd-ipojo-plugin-$VER.jar
+mvn deploy:deploy-file $REPO_VARS -Dpackaging=pom -DgroupId=org.apache.felix -DartifactId=ipojo-manipulator-bom -Dversion=$VER -Dfile=$JAR_DIR/ipojo-manipulator-bom-$VER.xml
+mvn deploy:deploy-file $REPO_VARS -Dpackaging=jar -DgroupId=org.apache.felix -DartifactId=org.apache.felix.ipojo -Dversion=$VER -Dfile=$JAR_DIR/org.apache.felix.ipojo-$VER.jar
+mvn deploy:deploy-file $REPO_VARS -Dpackaging=jar -DgroupId=org.apache.felix -DartifactId=org.apache.felix.ipojo.manipulator -Dversion=$VER -Dfile=$JAR_DIR/org.apache.felix.ipojo.manipulator-$VER.jar
+mvn deploy:deploy-file $REPO_VARS -Dpackaging=jar -DgroupId=org.apache.felix -DartifactId=org.apache.felix.ipojo.annotations -Dversion=$VER -Dfile=$JAR_DIR/org.apache.felix.ipojo.annotations-$VER.jar
+mvn deploy:deploy-file $REPO_VARS -Dpackaging=jar -DgroupId=org.apache.felix -DartifactId=maven-ipojo-plugin -Dversion=$VER -Dfile=$JAR_DIR/maven-ipojo-plugin-$VER.jar
+mvn deploy:deploy-file $REPO_VARS -Dpackaging=jar -DgroupId=org.apache.felix -DartifactId=bnd-ipojo-plugin -Dversion=$VER -Dfile=$JAR_DIR/bnd-ipojo-plugin-$VER.jar
